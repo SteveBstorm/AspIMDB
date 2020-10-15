e9a09abed558c12ba3215f9eb200367b89643eb1
elean <steve.lorent@gmail.com>
2020-10-15 22:09:31 +0200

    First push

---
+++ MovieDB/Tables/Actor.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Actor]
+(
+	[PersonID] INT NOT NULL, 
+    [MovieID] INT NOT NULL, 
+    [Role] VARCHAR(50) NOT NULL 
+    CONSTRAINT FK_Person_Actor FOREIGN KEY (PersonID) REFERENCES Person(Id),
+    CONSTRAINT FK_Person_Movie FOREIGN KEY (MovieID) REFERENCES Movie(Id)
+)
