202a643d45cdae271b1949f9010f571878cd02f7
SteveBstorm <65895628+SteveBstorm@users.noreply.github.com>
2020-11-03 11:56:22 +0100

    Add files via upload

--- LocalModel/Models/CompletePerson.cs
+++ LocalModel/Models/CompletePerson.cs
@@ -4,10 +4,13 @@ using System.Text;
 
 namespace LocalModel.Models
 {
-    public class Person
+    public class CompletePerson
     {
         public int Id { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+
+        public IEnumerable<Movie> RealMovies { get; set; }
+        public IEnumerable<Movie> ScenMovies { get; set; }
     }
 }
